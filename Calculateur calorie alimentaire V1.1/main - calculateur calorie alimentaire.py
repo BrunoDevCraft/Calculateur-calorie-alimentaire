e7e4675ec0f02bd8f8053ebc966bbc4bdba1db06
Bruno <151056141+BrunoDevCraft@users.noreply.github.com>
2026-04-21 13:01:22 +0200

    Delete Calculateur calorie alimentaire V1.1 directory

--- Calculateur calorie alimentaire V1.1/main - calculateur calorie alimentaire.py
+++
@@ -1,9 +0,0 @@
-# main.py
-
-import tkinter as tk
-from start_window import show_start_window
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()  # Masquer la fenêtre racine
-    show_start_window(root) 
